28ac8731a1dda5157840125a34873092841b2b24
gauravsh0812 <gauravs@arizona.edu>
2022-11-29 17:44:13 -0700

    updating new code

--- cnn.R
+++ cnn.R
@@ -14,38 +14,110 @@ library(readr)
 library(purrr)
 library(caret)
 
-# preprocessing images for CNN
-library(e1071)
-cifar <- dataset_cifar10()
 
-preprocess_image <- function(image){
-    width <- 100
-    height <- 100
-    # resize 
-    image <- resizeImage(image, w = width, h = height)
-    # padding
-    image <- padding(image, 150, 150, fill_value=0)
-    image <- image$data
-    # normalizing between 0 to 1
-    im <- im/255
-    
-    return(image)
+# Download the dataset first from the links provided in the csv file
+# we will be using 1000 images for now. One can increase the dataset 
+# size as per the requirement.
+
+set.seed(42)
+
+num_samples = 50
+jelly = read.csv("/Users/gaurav/Desktop/Education/M-574/semesterproject/jellyfish_t14.csv")
+# shuffling the dataset and take n_samples out of it
+jelly = jelly[sample(1:nrow(jelly)),][0:num_samples,]
+links= jelly[,4]
+dst_dir = "/Users/gaurav/Desktop/Education/M-574/semesterproject/M574_project/images/"
+for (n in 1:num_samples){
+  link = links[n]
+  print(paste(dst_dir,n,".jpg"))
+  download.file(link, destfile = paste(dst_dir,n,".jpg", sep=""),
+                method = "wget", extra = "-r -p --random-wait")
 }
 
-# read csv file having images_path and labels
-train_data <- scale(cifar$train$x)
-dim(train_data) <- c(50000,32,32,3)
+# split the dataset into train, test
+taxon_name = jelly[,5]
+images = paste0(dst_dir, seq(1:num_samples), ".jpg")
+df <- do.call(rbind.data.frame, Map('c', images, taxon_name))
+colnames(df)[1] <- "image_path"
+colnames(df)[2] <- "taxon_name"
+# labeling: text labels to categorical 
+numeric_labels = unclass(as.factor(df$taxon_name))
+df = cbind(df, numeric_labels)
+# check first 5 samples 
+head(df, n=5)
 
-test_data <- scale(cifar$test$x)
+# Let's split the dataset into train, and test
+# 80% train 20% test
+sample = sample(c(TRUE, FALSE), nrow(df), replace=TRUE, prob=c(0.8,0.2))
+train.df = df[sample, ]
+test.df = df[!sample, ]
+
+train.df.image_paths = train.df$image_path
+train.df.labels = train.df$numeric_labels
+test.df.image_paths = test.df$image_path
+test.df.labels = test.df$numeric_labels
+
+# preprocess the images
+preprocess_image <- function(image){
+  width <- 50
+  height <- 50
+  # resize 
+  image <- resizeImage(image, w = width, h = height)
+  # padding
+  image <- padding(image, 150, 150, fill_value=0)
+  image <- image$data
+  # normalizing between 0 to 1
+  image <- image/255
+  
+  return(image)
+}
+
+calling_fn <- function(img){
+  tryCatch(
+
+    {
+      image=readImage(img)
+      image=preprocess_image(image)
+      return(image)
+    },
+    #if an error occurs, tell me the error
+    error=function(e) {
+      message('An Error Occurred')
+      print(e)
+    },
+    #if a warning occurs, tell me the warning
+    warning=function(w) {
+      message('A Warning Occurred')
+      print(w)
+      return(NA)
+    }
+  )
+}
+
+image.array = c()
+count=0
+for (img in train.df.image_paths){
+  print(img)
+  image = calling_fn(img)
+  print(image[1])
+  ifelse(is.na(image), print("Discarding Image!"), {count=count+1})
+  
+  #image = readImage(img)
+  #image = my_function(image)
+  image.array = c(image.array, image)
+}
+
+image.array = array(data = image.array, dim= c(count, 150, 150, 3))
+
+dim(train_data) <- c(num_samples,32,32,3)
 dim(test_data) <- c(10000,32,32,3)
-
-train_label <- as.numeric(cifar$train$y)
 dim(train_label) <- c(50000)
-
-test_label <- as.numeric(cifar$test$y)
 dim(test_label) <- c(10000)
 
 #######################################################################
+# preprocessing images for CNN
+#library(e1071)
+#cifar <- dataset_cifar10()
 
 class_names <- c('airplane', 'automobile', 'bird', 'cat', 'deer',
                  'dog', 'frog', 'horse', 'ship', 'truck')
@@ -68,7 +140,7 @@ model <- keras_model_sequential() %>%
   layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = "relu", input_shape = c(32,32,3)) %>% 
   layer_max_pooling_2d(pool_size = c(2,2)) %>% 
   layer_batch_normalization() %>%
-  layer_conv_2d(filters = 64, kernel_size = c(3,3), activation = "relu") %>% 
+  l1ayer_conv_2d(filters = 64, kernel_size = c(3,3), activation = "relu") %>% 
   layer_max_pooling_2d(pool_size = c(2,2)) %>% 
   layer_batch_normalization() %>%
   layer_conv_2d(filters = 64, kernel_size = c(3,3), activation = "relu") %>%
